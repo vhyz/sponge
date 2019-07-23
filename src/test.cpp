0360a3f0dd16e2e97a64e0b63531456957644831
vhyz <32564682@qq.com>
2019-07-23 20:30:45 +0800

    update readme.md

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,20 @@
-#include"HttpTestServer.h"
+#include "TcpServer.h"
+
+void echo(const spTcpConnection& spConn, std::string& msg) {
+    std::string s;
+    s.swap(msg);
+    spConn->send(s);
+}
 
 int main() {
+    // 首先创建一个EventLoop
     EventLoop loop;
 
-    HttpTestServer httpTestServer(&loop,5000,7);
+    // 随后创建一个服务器，后两个参数分别为端口和IO线程数量
+    TcpServer tcpServer(&loop, 5000, 7);
 
-    httpTestServer.start();
+    tcpServer.setMsgCallBack(
+        std::bind(&echo, std::placeholders::_1, std::placeholders::_2));
+
+    tcpServer.start();
 }
